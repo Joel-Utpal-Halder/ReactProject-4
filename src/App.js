35452ed86595ab6427a275404d82bfd91a631fc8
Your Name <you@example.com>
2024-09-20 22:55:53 +0600

    Live Website Link Added Taking from Netlify

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ function App() {
 // # Lacture : Pass multiple data to a child component, Props and Destructuring
 // Worked on Country.js and countires.js file
 
+// # Lecture : Responsive design, Build react app and project host in netlify
+// To upload the project in GitHub and run in Netlify is shown in this lacture
+// Running the project in Netlify is different for React project as there is no HTML file to run, and not possible to run as GitHub live website
+// 'npm run build' to write in Terminal to build the project and a folder name 'build' will appear in the react project folder in the pc, which is a minified version of the project to run in Netlify
+// Under 'package.json' folder 'start, build ect. are showing under 'script'
 
 
 export default App;
